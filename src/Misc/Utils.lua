689e29114fc49fedbc53c3ed60f93bf0aa22322a
Chris <peavers@gmail.com>
2024-06-26 16:09:13 -0500

    fix: Correct reload prompt not displaying after applying profiles

--- src/Misc/Utils.lua
+++ src/Misc/Utils.lua
@@ -1,4 +1,7 @@
-function ShowReloadUIPopup()
+local addonName, addonTable = ...
+addonTable.UI = addonTable.UI or {}
+
+function addonTable.UI.ShowReloadUIPopup()
     StaticPopupDialogs["RELOAD_UI"] = {
         text = "Settings have been applied. You need to reload the UI for changes to take effect. Do you want to reload now?",
         button1 = "Yes",
@@ -13,3 +16,8 @@ function ShowReloadUIPopup()
     }
     StaticPopup_Show("RELOAD_UI")
 end
+
+function addonTable.UI.ReloadUIAfterApplying()
+    addonTable.UI.ShowReloadUIPopup()
+end
+
